365de56d90f9f9ada1ba8a40896bcd8d937f14b7
[Matyas Hub] <[hubmatyas@gmail.com]>
2022-01-15 21:54:26 +0100

    Fixed font import

--- header.php
+++ header.php
@@ -26,7 +26,11 @@
 				}?>
 		</title>
 		<meta name="description" content="Poskytujeme psychiatrickou, psychologickou a psychoterapeutickou péči. Díky naší kapacitě můžeme registrovat nové pacienty.">
-		<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
+
+<!-- Connect Google Fonts (Poppins) -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
 
 		<!-- OpenGraph -->
 		<meta name="content-language" content="cs">
