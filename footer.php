3888b291377abcd3fc1df4d4abed52cee3e3f08d
[Matyas Hub] <[hubmatyas@gmail.com]>
2022-01-15 22:42:37 +0100

    Few minor fixes

--- footer.php
+++ footer.php
@@ -20,7 +20,12 @@
         <p class="consent-text">Naše webové stránky používají k&nbsp;poskytování služeb a&nbsp;analýze návštěvnosti soubory cookie. Používáním souhlasíte s&nbsp;použitím souborů cookie.</p><div class="consent-buttons"><span id="accept">Souhlasím</span><a class="more-info" href="/gdpr">Zjistit&nbsp;více...</a></div>
 </div>
 
+    <?php if (is_front_page()):?>
+	<script
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBUEVlByzT8CL183iWpsB3yKLbSfHZ4qFM&map_ids=54220ee84d2e31ca" async></script>
+<?php endif; ?>
   <script src="/wp-content/themes/psychiatric/res/main.js"></script>
+</script>
 </body>
 </html>
 
--- front-page.php
+++ front-page.php
@@ -24,9 +24,14 @@
 </section>
 	-->
 	 
+  <?php
+   if (have_posts()) : while(have_posts()) : the_post();
+   the_content();
+   endwhile;endif;
+  ?>
 
-  <section class="dashboard">
     <h3 class="commonTitle">O nás - Psychiatrie - Ambulance s.r.o.</h3>
+  <section class="dashboard">
     <div>
       <img width="200" height="200" src="/wp-content/themes/psychiatric/res/svg/icons/brain.svg" alt="Ikona - Poskytujeme psychiatrickou, psychologickou a psychoterapeutickou péči.">
       <p>Poskytujeme psychiatrickou, psychologickou a psychoterapeutickou péči.</p>
@@ -51,83 +56,8 @@
 	
 	
 	<h3 class="commonTitle">Kde nás najdete?</h3>
+
 	<div id="map"></div>
-	<script
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBUEVlByzT8CL183iWpsB3yKLbSfHZ4qFM&map_ids=54220ee84d2e31ca" async>
-</script>
-	<script>
-		
-    function initMap(){
-      // Map options
-      var options = {
-        zoom:10,
-        center:{lat:49.8179884,lng:13.3920277},
-		  mapId: '54220ee84d2e31ca'
-      }
-
-      var map = new google.maps.Map(document.getElementById('map'), options);
-
-
-      var markers = [
-        {
-          coords:{lat:49.7536835,lng:13.3769782},
-              content:'<div class="map-box"><strong>Plzeň, Rondel</strong><p>Lochotínská 18, 301 00 Plzeň 1</p><a href="">Zobrazit v mapách google</a></div>'
-        },
-        {
-          coords:{lat:49.7307896,lng:13.3986162},
-          content:'<div class="map-box"><strong>Plzeň, Slovany</strong><p>Lochotínská 18, 301 00 Plzeň 1</p><a href="">Zobrazit v mapách google</a></div>'
-        },
-        {
-          coords:{lat:49.8179884,lng:13.3920277},
-          content:'<div class="map-box"><strong>Třemošná</strong><p>Zálužská 138, 330 11 Třemošná</p><a href="">Zobrazit v mapách google</a></div>'
-        },
-        {
-          coords:{lat:49.9346361,lng:13.3992181},
-          content:'<div class="map-box"><strong>Plasy</strong><p>Potoční 546, 331 01 Plasy</p><a href="">Zobrazit v mapách google</a></div>',
-        }
-      ];
-
-
-      for(var i = 0;i < markers.length;i++){
-        addMarker(markers[i]);
-      }
-
-      function addMarker(props){
-        var marker = new google.maps.Marker({
-          position:props.coords,
-          map:map,
-        });
-
-        if(props.iconImage){
-          marker.setIcon(props.iconImage);
-        }
-
-        if(props.content){
-          var infoWindow = new google.maps.InfoWindow({
-            content:props.content
-          });
-
-          marker.addListener('click', function(){
-            infoWindow.open(map, marker);
-          });
-        }
-      }
-    }
-		
-window.addEventListener('load', function() {
-   initMap();
-});
-  </script>
-
-
-
-
-
-  <?php
-   if (have_posts()) : while(have_posts()) : the_post();
-   the_content();
-   endwhile;endif;
-  ?>
 
 </div> <!-- wrapper -->
 
