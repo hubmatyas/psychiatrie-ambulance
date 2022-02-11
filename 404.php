a0541ccaeb5aeb3ac5446eeba36a5e54dab926ec
[Matyas Hub] <[hubmatyas@gmail.com]>
2022-02-11 13:46:57 +0100

    Added an error page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+Template Name: Error page
+
+*/
+
+
+get_header(); ?>
+
+
+<div class="wrapper">
+
+  <h1 class="title">Omlouváme se</h1>
+  <h2 class="subtitle">Na této adrese jsme nic nenalezli</h2>
+<br>
+<p>Zpět na <a href="<?php echo get_home_url(); ?>">psychiatrie-ambulance.cz</a></p>
+
+</div>
+
+<?php get_footer(); ?>
